af1a5fc741a9c7b23ef9455c8c636e2d92f235aa
Mike Stevens <smartguy1044@gmail.com>
2017-05-21 21:14:12 -0400

    Add routing to material2 tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent }   from './views/home.component';
+import { AboutComponent }  from './views/about.component';
+import { ProjectsComponent }  from './views/projects.component';
+import { PostListComponent } from './views/posts/post-list/post-list.component';
+import {PostSingleComponent} from './views/posts/post-single/post-single.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+  {
+    path: 'blog',
+    component: PostListComponent
+  },
+  {
+    path: ':slug',
+    component: PostSingleComponent
+  }
+];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.template.html'
+  templateUrl: './app.template.html',
+  styleUrls: ['./app.component.css']
 })
 
-export class AppComponent  { }
+export class AppComponent implements OnInit {
+  routeLinks: any[];
+  activeLinkIndex: number;
+
+  constructor(private router: Router) {
+    this.routeLinks = [
+      {label: 'Home', link: ''},
+      {label: 'About', link: 'about'},
+      {label: 'Projects', link: 'projects'},
+      {label: 'Blog', link: 'blog'}
+    ];
+  }
+
+  ngOnInit() {
+    this.activeLinkIndex = 1;
+    this.router.navigate(['']);
+  }
+}
