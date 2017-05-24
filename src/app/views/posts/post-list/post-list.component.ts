a4378b5366c25f3fff190a466e8ed02a2aca4a49
Mike Stevens <smartguy1044@gmail.com>
2017-05-23 20:27:07 -0400

    Add subtitle to title and style it

--- src/app/views/posts/post-list/post-list.component.ts
+++ src/app/views/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Post } from '../post';
@@ -38,6 +38,10 @@ export class PostListComponent implements OnInit {
 
   selectPost(slug) {
     this.router.navigate([slug]);
+  }
+
+  transform(input: string, index: number) {
+    return input.split('<br>')[index];
   }
 
 }
