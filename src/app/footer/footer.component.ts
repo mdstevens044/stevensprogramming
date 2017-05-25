248ed2c6096faae28585d494ff6e9dfdc07bb5cc
Mike Stevens <smartguy1044@gmail.com>
2017-05-25 15:48:22 -0400

    Add footer specific css file

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'footer-generic',
-  templateUrl: './footer.template.html'
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
 })
 
 export class FooterComponent {
