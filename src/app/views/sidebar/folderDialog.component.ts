1251de0d07e111f2d4b5f939c1d78063640d96fb
Mike Stevens <smartguy1044@gmail.com>
2017-06-19 00:03:39 -0400

    Try and fix not adding folder

--- src/app/views/sidebar/folderDialog.component.ts
+++ src/app/views/sidebar/folderDialog.component.ts
@@ -1,22 +1,27 @@
-import {Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input } from '@angular/core';
 import { Sidebar } from './sidebar';
 import { SidebarService } from './sidebar.service';
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './folderDialog.component.html',
-  styleUrls: ['./folderDialog.component.css'],
-  providers: [SidebarService]
+  styleUrls: ['./folderDialog.component.css']
 })
 
 export class DialogComponent implements OnInit {
 
-  sidebars: Sidebar[];
+  @Input()
+  sidebar: Sidebar;
+
+  @Input()
+  createHandler: Function;
 
   constructor(private sidebarService: SidebarService) {}
 
-  addFolders(element) {
-    this.sidebarService.createFolders(element.value);
+  addFolders(sidebar: Sidebar) {
+    this.sidebarService.createFolders(sidebar).then((newFolder: Sidebar) => {
+      this.createHandler(newFolder);
+    });
   }
 
   ngOnInit() {
