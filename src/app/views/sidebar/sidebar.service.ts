d71797c71fc1a522a2e99f6a2e4a4911749798d1
Mike Stevens <smartguy1044@gmail.com>
2017-06-19 22:36:04 -0400

    Change service

--- src/app/views/sidebar/sidebar.service.ts
+++ src/app/views/sidebar/sidebar.service.ts
@@ -17,7 +17,7 @@ export class SidebarService {
       .catch(this.handleError);
   }
 
-  createFolders(newContact) {
+  createFolders(newContact: Sidebar) {
     return this.http.post(this.foldersUrl, newContact)
       .map((res: Response) => res.json())
       .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
