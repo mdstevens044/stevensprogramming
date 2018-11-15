d579377c53fa8651dc5e87b9aef5b96bff0560ce
mdstevens <mdstevens044@gmail.com>
2018-11-14 22:03:21 -0500

    Move the about me content to graphcms so I can update content without having to rebuild the project

--- src/app/views/about/about.component.ts
+++ src/app/views/about/about.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html'
 })
 
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  abouts: any;
+  loading = true;
 
+  constructor( private apollo: Apollo ) { }
+
+  ngOnInit() {
+    this.apollo
+      .use('graphCms')
+      .watchQuery<any>({
+        query: gql`
+          {
+            abouts
+            {
+              about
+            }
+          }
+        `
+      })
+      .valueChanges.subscribe(({ data, loading }) => {
+      this.loading = loading;
+      this.abouts = data.abouts;
+    });
+  }
+
+  replaceLineBreaks(replaceStr: string)
+  {
+    return replaceStr.replace(new RegExp('\r?\n','g'), '<br>');
+  }
 }
