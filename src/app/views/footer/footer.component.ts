6713be398e8e3d3e8b925c8762b44398ee50f45e
mdstevens <Dsn4uf13>
2019-03-26 14:11:25 -0400

    Fix all component selector warning messages

--- src/app/views/footer/footer.component.ts
+++ src/app/views/footer/footer.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'footer-generic',
+  selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
