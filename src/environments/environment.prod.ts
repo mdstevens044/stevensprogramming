1a46a5379b63004973a27aa50b8449f3fb172e22
mdstevens <Dsn4uf13>
2019-08-27 16:00:38 -0400

    Remove environments files from .gitignore

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  ghUserName: 'mdstevens044',
+  ghEndPoint: 'https://api.github.com/graphql?access_token=fd7c944fb59f74960a5e9fd0ad247f5b62e7d856',
+  wpBase: 'https://stevensprogramming.com/blog/wp-json/wp/v2/',
+  graphCms: 'https://api-useast.graphcms.com/v1/cjnb3itxs1g0v01ghlqpw9o5a/master',
+  recaptchaSiteKey: '6LceN5oUAAAAANc5n7pAog1xLTfAOI-2K8eyQ9zP',
+  emailApi: 'https://api.stevensprogramming.com/api/email/send'
+};
